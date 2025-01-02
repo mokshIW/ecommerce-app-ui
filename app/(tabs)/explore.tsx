3a639d48d60ab05c8325cc1cafb3e0cd72c26597
Moksh Keloo <moksh@integratedwebworks.com>
2025-01-02 18:27:57 -0500

    Explore Screen UI done.

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { CategoryType } from "@/types/type";
+import { Stack } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Colors } from "@/constants/Colors";
 
 type Props = {};
 
 const ExploreScreen = (props: Props) => {
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const headerHeight = useHeaderHeight();
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = async () => {
+    const URL = "http://localhost:8000/categories";
+    const response = await axios.get(URL);
+    // console.log(response.data);
+    setCategories(response.data);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Explore Screen</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
+      <View style={[styles.container, { marginTop: headerHeight }]}>
+        <FlatList
+          data={categories}
+          keyExtractor={(item) => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <View key={index} style={styles.itemWrapper}>
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 100, height: 100, borderRadius: 10 }}
+              />
+              <Text style={styles.itemTitle}>{item.name}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </>
   );
 };
 
@@ -16,7 +51,20 @@ export default ExploreScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  itemWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    backgroundColor: Colors.lighterGray,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  itemTitle: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: Colors.black,
   },
 });
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,9 +1,10 @@
 export const Colors = {
-  primary: '#572fff',
-  black: '#333',
-  gray: '#666',
-  lightGray: '#999',
-  white: '#fff',
-  background: '#F4F4F4',
-  highlight: '#F4CE14',
-}
+  primary: "#572fff",
+  black: "#333",
+  gray: "#666",
+  lightGray: "#999",
+  lighterGray: "#e5e5e5",
+  white: "#fff",
+  background: "#F4F4F4",
+  highlight: "#F4CE14",
+};
